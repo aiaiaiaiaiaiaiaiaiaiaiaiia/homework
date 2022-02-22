2e18d83cd2b26ed1b1ec010b9d2d2caa6a8204b4
unknown <maksatovaaidana05@gmail.com>
2022-02-22 15:05:30 +0600

    ok

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+
+let some = prompt ("Type your birth year");
+let year = 2022
+let age = year - some
+ if  ( some > year ){
+     alert ("ты еще не родился") 
+    }
+  else if ( age < 30 ){
+      alert ("вы молодой")
+  }
+  else if ( age > 60 ){
+      alert ("да ты динозавр")
+  }
+else if( age < 60) {
+   alert ("у вас кризис среднего восзраста") 
+}
+
+
+
